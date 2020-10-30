86a31aff09055ab7eed3149b4f063e8f6429b659
Abel Moro <abelmoro@ucm.es>
2020-10-30 14:56:55 +0100

    Settings patch

---
+++ Source/GunNutCommunity/GNCPDefDisabler.cs
@@ -0,0 +1,169 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace GunNutCommunity
+{
+	[StaticConstructorOnStartup]
+	public static class GNCPDefDisabler
+	{
+		static GNCPDefDisabler() => GNCPDefDisabler.DefDisablerList();
+
+		public static void DefDisablerList()
+		{
+			bool value1 = !GNCPMod.settings.enable_GNCP_KS23;
+			if (value1)
+			{
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_KSTwentyThree"));
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_KSTwentyThree"));
+			}
+
+			bool value2 = !GNCPMod.settings.enable_GNCP_P90;
+			if (value2)
+			{
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_PNinety"));
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_PNinety"));
+			}			
+
+			bool value3 = !GNCPMod.settings.enable_GNCP_SR25;
+			if (value3)
+			{
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_SRTwentyFive"));
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_SRTwentyFive"));
+			}
+
+			bool value4 = !GNCPMod.settings.enable_GNCP_GalilACE;
+			if (value4)
+			{
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_GalilACE"));
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_GalilACE"));
+			}
+
+			//bool value5 = !GNCPMod.settings.enable_GNCP_AT4;
+			//if (value5)
+			//{
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_ATFour"));
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_ATFour"));
+			//}
+
+			bool value6 = !GNCPMod.settings.enable_GNCP_StG44;
+			if (value6)
+			{
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_StGFortyFour"));
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_StGFortyFour"));
+			}		
+
+			bool value8 = !GNCPMod.settings.enable_GNCP_MG3;
+			if (value8)
+			{
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_MGThree"));
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_MGThree"));
+			}
+
+			bool value9 = !GNCPMod.settings.enable_GNCP_G98;
+			if (value9)
+			{
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_GlockFLUX"));
+				GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_GlockFLUX"));
+			}		
+
+			//bool value10 = !GNCPMod.settings.enable_GNCP_M16A4;
+			//if (value10)
+			//{
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_M16A4"));
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_M16A4"));
+			//}
+
+			//bool value11 = !GNCPMod.settings.enable_GNCP_M2HB;
+			//if (value11)
+			//{
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_M2HB"));
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_M2HB"));
+			//}
+
+			//bool value12 = !GNCPMod.settings.enable_GNCP_GreyHawk;
+			//if (value12)
+			//{
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_GreyHawk"));
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_GreyHawk"));
+			//}	
+
+			//bool value13 = !GNCPMod.settings.enable_GNCP_AA12;
+			//if (value13)
+			//{
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_AATwelve"));
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_AATwelve"));
+			//}
+
+			//bool value14 = !GNCPMod.settings.enable_GNCP_M202;
+			//if (value14)
+			//{
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_M202F"));
+			//	GNCPDefDisabler.DisableDef(ThingDef.Named("GN_Bullet_M202F"));
+			//}
+		}
+
+		public static void DisableDef(ThingDef def)
+		{
+			List<ResearchProjectDef> researchPrerequisites = def.researchPrerequisites;
+
+			if (researchPrerequisites != null)
+			{
+				researchPrerequisites.Clear();
+			}
+
+			List<string> weaponTags = def.weaponTags;
+
+			if (weaponTags != null)
+			{
+				weaponTags.Clear();
+			}
+
+			def.deepCommonality = 0f;
+
+			def.generateCommonality = 0f;
+
+			def.tradeability = Tradeability.None;
+
+			List<ThingCategoryDef> thingCategories = def.thingCategories;
+
+			if (thingCategories != null)
+			{
+				thingCategories.Clear();
+			}
+
+			List<ThingCategoryDef> thingCategories2 = def.thingCategories;
+
+			if (thingCategories2 != null)
+			{
+				thingCategories2.Add(ThingCategoryDefOf.Chunks);
+			}
+
+			foreach (RecipeDef recipeDef in DefDatabase<RecipeDef>.AllDefsListForReading)
+			{
+				bool value1 = recipeDef.ProducedThingDef == def;
+				if (value1)
+				{
+					List<ThingDef> recipeUsers = recipeDef.recipeUsers;
+					if (recipeUsers != null)
+					{
+						recipeUsers.Clear();
+					}
+					List<ResearchProjectDef> researchPrerequisites2 = recipeDef.researchPrerequisites;
+					if (researchPrerequisites2 != null)
+					{
+						researchPrerequisites2.Clear();
+					}
+					recipeDef.researchPrerequisite = null;
+				}
+			}
+
+			bool value2 = DefDatabase<ThingDef>.AllDefsListForReading.Contains(def);
+
+			if (value2)
+			{
+				DefDatabase<ThingDef>.AllDefsListForReading.Remove(def);
+			}
+		}
+	}
+}
---
+++ Source/GunNutCommunity/GNCPSettings.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace GunNutCommunity
+{
+	internal class GNCPSettings : ModSettings
+	{
+		/// <summary>
+		/// The settings our mod has.
+		/// </summary>
+		public bool enable_GNCP_KS23 = true;
+		public bool enable_GNCP_P90 = true;
+		public bool enable_GNCP_SR25 = true;
+		public bool enable_GNCP_GalilACE = true;
+		//public bool enable_GNCP_AT4 = true;
+		public bool enable_GNCP_StG44 = true;
+		public bool enable_GNCP_MG3 = true;
+		public bool enable_GNCP_G98 = true;
+		//public bool enable_GNCP_M16A4 = true;
+		//public bool enable_GNCP_M2HB = true;
+		//public bool enable_GNCP_GreyHawk = true;
+		//public bool enable_GNCP_AA12 = true;
+		//public bool enable_GNCP_M202 = true;
+
+		public bool enable_GNCP_nicknames = true;
+
+		public void ResetSavedDefs()
+		{
+			this.enable_GNCP_KS23 = true;
+			this.enable_GNCP_P90 = true;
+			this.enable_GNCP_SR25 = true;
+			this.enable_GNCP_GalilACE = true;
+			//this.enable_GNCP_AT4 = true;
+			this.enable_GNCP_StG44 = true;
+			this.enable_GNCP_MG3 = true;		
+			this.enable_GNCP_G98 = true;
+			//this.enable_GNCP_M16A4 = true;
+			//this.enable_GNCP_M2HB = true;
+			//this.enable_GNCP_GreyHawk = true;
+			//this.enable_GNCP_AA12 = true;
+			//this.enable_GNCP_M202 = true;
+
+			this.enable_GNCP_nicknames = true;
+		}
+
+		/// <summary>
+		/// The part that writes our settings to file. Note that saving is by ref.
+		/// </summary>
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.Look<bool>(ref this.enable_GNCP_KS23, "enable_GNCP_KS23", true, false);
+			Scribe_Values.Look<bool>(ref this.enable_GNCP_P90, "enable_GNCP_P90", true, false);
+			Scribe_Values.Look<bool>(ref this.enable_GNCP_SR25, "enable_GNCP_SR25", true, false);
+			Scribe_Values.Look<bool>(ref this.enable_GNCP_GalilACE, "enable_GNCP_GalilACE", true, false);
+			//Scribe_Values.Look<bool>(ref this.enable_GNCP_AT4, "enable_GNCP_AT4", true, false);
+			Scribe_Values.Look<bool>(ref this.enable_GNCP_StG44, "enable_GNCP_StG44", true, false);
+			Scribe_Values.Look<bool>(ref this.enable_GNCP_MG3, "enable_GNCP_MG3", true, false);			
+			Scribe_Values.Look<bool>(ref this.enable_GNCP_G98, "enable_GNCP_G98", true, false);
+			//Scribe_Values.Look<bool>(ref this.enable_GNCP_M16A4, "enable_GNCP_M16A4", true, false);			
+			//Scribe_Values.Look<bool>(ref this.enable_GNCP_M2HB, "enable_GNCP_M2HB", true, false);
+			//Scribe_Values.Look<bool>(ref this.enable_GNCP_GreyHawk, "enable_GNCP_GreyHawk", true, false);
+			//Scribe_Values.Look<bool>(ref this.enable_GNCP_AA12, "enable_GNCP_AA12", true, false);
+			//Scribe_Values.Look<bool>(ref this.enable_GNCP_M202, "enable_GNCP_M202", true, false);
+
+			Scribe_Values.Look<bool>(ref this.enable_GNCP_nicknames, "enable_GNCP_nicknames", true, false);
+		}
+
+		public void DoSettingsWindowContents(Rect settingsCanvas)
+		{
+			Listing_Standard list = new Listing_Standard
+			{
+				ColumnWidth = (settingsCanvas.width - 17) / 2
+			};
+
+			list.Begin(settingsCanvas);
+
+			Text.Font = GameFont.Medium;
+			list.Label("settings_GNCP_EnableHeader".Translate());
+			list.GapLine();
+
+			Text.Font = GameFont.Small;
+			list.Label("settings_GNCP_EnableDescription".Translate());
+			list.Gap();
+
+			list.CheckboxLabeled("enable_GNCP_KS23".Translate(), ref this.enable_GNCP_KS23, null);
+			list.CheckboxLabeled("enable_GNCP_P90".Translate(), ref this.enable_GNCP_P90, null);
+			list.CheckboxLabeled("enable_GNCP_SR25".Translate(), ref this.enable_GNCP_SR25, null);
+			list.CheckboxLabeled("enable_GNCP_GalilACE".Translate(), ref this.enable_GNCP_GalilACE, null);
+			list.Label("Because of how is implemented the AT4 cannot be disabled right now");
+			//list.CheckboxLabeled("enable_GNCP_AT4".Translate(), ref this.enable_GNCP_AT4, null);
+			list.CheckboxLabeled("enable_GNCP_StG44".Translate(), ref this.enable_GNCP_StG44, null);
+			list.CheckboxLabeled("enable_GNCP_MG3".Translate(), ref this.enable_GNCP_MG3, null);			
+			list.CheckboxLabeled("enable_GNCP_G98".Translate(), ref this.enable_GNCP_G98, null);
+			//list.CheckboxLabeled("enable_GNCP_M16A4".Translate(), ref this.enable_GNCP_M16A4, null);			
+			//list.CheckboxLabeled("enable_GNCP_M2HB".Translate(), ref this.enable_GNCP_M2HB, null);
+			//list.CheckboxLabeled("enable_GNCP_GreyHawk".Translate(), ref this.enable_GNCP_GreyHawk, null);
+			//list.CheckboxLabeled("enable_GNCP_AA12".Translate(), ref this.enable_GNCP_AA12, null);
+			//list.CheckboxLabeled("enable_GNCP_M202".Translate(), ref this.enable_GNCP_M202, null);
+			list.Gap();
+			list.Gap();
+			list.Gap();
+			list.Gap();
+			list.Gap();
+
+			list.Label("settings_GNCP_EndDescription".Translate());
+
+			list.NewColumn();
+
+			Text.Font = GameFont.Medium;
+			list.Label("settings_GNCP_EnableNickNameHeader".Translate());
+			list.GapLine();
+
+			Text.Font = GameFont.Small;
+			list.Label("settings_GNCP_EnableNickNameDescription".Translate());
+			list.Gap();
+
+			list.CheckboxLabeled("enable_GNCP_nicknames".Translate(), ref this.enable_GNCP_nicknames, null);
+
+			list.End();
+		}
+	}
+}
